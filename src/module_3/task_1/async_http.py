cf7577d474d3a708d351d6bfb55c893ee70e006f
LenaNS <lena.noskova.99@mail.ru>
2025-02-21 23:14:54 +0800

    правки

--- src/module_3/task_1/async_http.py
+++ src/module_3/task_1/async_http.py
@@ -5,7 +5,6 @@ from typing import Tuple
 from aiohttp import ClientSession
 
 
-
 async def fetch_url(
     session: ClientSession, url: str, semaphore: int
 ) -> Tuple[str, int]:
@@ -20,11 +19,11 @@ async def fetch_url(
 async def fetch_urls(urls: list[str], file_path: str) -> None:
     semaphore = asyncio.Semaphore(5)
     for url in urls:
-         async with ClientSession() as session:
+        async with ClientSession() as session:
             task = await fetch_url(session, url, semaphore)
             with open(file_path, "a") as file:
-                    json.dump({"url": task[0], "status_code": task[1]}, file)
-                    file.write("\n")
+                json.dump({"url": task[0], "status_code": task[1]}, file)
+                file.write("\n")
 
 
 urls = [
